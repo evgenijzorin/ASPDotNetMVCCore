7a755c51b5f5edfdbb60de683330f98499a65d5f
“[Зорин <[evgenijzorin@bk.ru]>
2022-06-06 19:20:08 +0300

    create task for lesson 1

---
+++ TaskLesson_1/TaskLesson_1/Controllers/CatalogController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TaskLesson_1.Data.Interfaces;
+using TaskLesson_1.ViewModels;
+
+namespace TaskLesson_1.Controllers
+{
+    public class CatalogController : Controller
+    {
+        // Переменные на интерфейсы
+        private readonly IAllProducts _allProducts;
+        private readonly ICategorys _categorys;
+
+        public CatalogController (IAllProducts iAllProducts, ICategorys iCategorys)
+        {
+            _allProducts = iAllProducts;
+            _categorys = iCategorys;
+        }
+
+        public ViewResult products()
+        {
+            // Передаем данные в класс для отображения
+            CatalogProductsViewModel obj = new CatalogProductsViewModel();
+            obj.allProducts = _allProducts.Products;
+            obj.currentCategory = "Канцелярские принадлежности";
+            return View(obj);
+        }
+
+
+    }
+}
---
+++ TaskLesson_1/TaskLesson_1/Data/Interfaces/IAllProducts.cs
@@ -0,0 +1,10 @@
+﻿using TaskLesson_1.Data.Models;
+
+namespace TaskLesson_1.Data.Interfaces
+{
+    public interface IAllProducts
+    {
+        IEnumerable<Product> Products { get; } // перечисление товаров
+        // Product GetProduct(int ProductId);     // Возвратить товар по ID
+    }
+}
