971ed829dfbbede6b21328a4670c2bdfa4391bc8
“[Зорин <[evgenijzorin@bk.ru]>
2022-06-14 23:00:12 +0300

    a working mvc model has been created but multithreading has not been implemented now

---
+++ TaskLesson_3/TaskLesson_3/Controllers/CatalogController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskLesson_3.Models;
+
+namespace TaskLesson_3.Controllers
+{
+    public class CatalogController : Controller
+    {
+        private static Catalog _catalog = new();
+        /// <summary>
+        /// Вывести категории
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult Categories()
+        {
+            return View(_catalog);
+        }
+        /// <summary>
+        /// Добавить категорию
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult Categories(Category model)
+        {
+            _catalog.Categories.Add(model);
+            return View(_catalog);
+        }
+
+        /// <summary>
+        /// Вывести продукты
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult Products()
+        {
+            return View(_catalog);
+        }
+
+        /// <summary>
+        /// Добавить продукт
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult Products(Product model)
+        {
+            _catalog.Products.Add(model);
+            return View(_catalog);
+        }
+
+
+
+
+
+    }
+}
---
+++ TaskLesson_3/TaskLesson_3/Models/Catalog.cs
@@ -0,0 +1,29 @@
+﻿namespace TaskLesson_3.Models
+{
+    public class Catalog
+    {                   
+        private List<Category> _categories { get; set; } = new();
+        private List<Product> _products { get; set; } = new();
+        /// <summary>  Получить все товары </summary>
+        public List<Product> Products { get { return _products; } }
+        /// <summary>  Получить все категории </summary>
+        public List<Category> Categories { get { return _categories; } }
+
+    }
+
+    public class Category
+    {
+        public int Id { get; set; }          
+        public string Name { get; set; }          
+    }
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public string? Image { get; set; }
+        public int CategoryId { get; set; }
+    }
+    
+}
